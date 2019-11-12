6ffe2a6389f18bfd37ba95734849cfc317547d4a
z18406565980 <130110945@qq.com>
2019-11-12 16:34:02 +0800

    h5

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function controlLight() {
+    var isOpen=true;
+    document.querySelector(".lightOff").onclick=function () {
+
+        document.querySelector(".index-fourth-out").style.display="none";
+        document.querySelector(".index-fourth-in").style.display="block";
+    };
+    document.querySelector(".music").onclick=function () {
+        isOpen=!isOpen;
+        if(isOpen===true){
+           document.querySelector(".musicImg").src="cmbChina%20demo/images/musicBtnOff.png";
+           document.querySelector(".audio").pause();
+        }else {
+            document.querySelector(".musicImg").src="cmbChina%20demo/images/musicBtn.png";
+            document.querySelector(".audio").play();
+        }
+    }
+}
+controlLight();
